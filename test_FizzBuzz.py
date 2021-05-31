4e448f081b1f2fedcda87d4d4ab33c1badec3182
Jonathan Ma <majo@oregonstate.edu>
2021-05-30 20:58:21 -0700

    Wrote second test, test output of program to see if it pritns number from 1-100 and if it is multiple of 3 then print fizz, if multiple of 5 print buzz and if multiple of both then print fizzbuzz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -20,6 +20,26 @@ class TestCaseFizzBuzz(unittest.TestCase):
 
             self.assertEqual(result, testedVal)
 
+    #Test if the numbers printed are from 1-100
+    # if it is a multiple of 3 then print Fizz
+    # If it is a multiple of 5 then print buzz
+    # If multiple of 3 and 5 then pritn FizzBuzz
+    def test_program(self):
+        with patch('sys.stdout', new = StringIO()) as printedNum:
+            FizzBuzz.FizzBuzz()
+            testedVal = ""
+            for i in range(1,101):
+                if( (i % 3 == 0) and (i % 5 == 0)):
+                    testedVal += "FizzBuzz"
+                elif (i % 3 == 0):
+                    testedVal += "Fizz"
+                elif (i % 5 == 0):
+                    testedVal += "Buzz"
+                else:
+                    testedVal += str(i)
+            result = (printedNum.getvalue()).replace('\n','')
+
+            self.assertEqual(result, testedVal)
 
 
 if __name__ == '__main__':
