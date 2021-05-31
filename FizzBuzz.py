bc6db3013a4e3e0d26dcaa3570d07caf3afc3620
Jonathan Ma <majo@oregonstate.edu>
2021-05-30 20:52:38 -0700

    Implemented print method that prints numbers in the range of 1 and 100 excluding numbers that are multiples of 3 and 5

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+def main():
+    FizzBuzz
+
+def FizzBuzz():
+    for i in range(1,101):
+        if( i % 3 == 0):
+            pass
+        elif(i % 5 == 0):
+            pass
+        else:
+            print(i)
+
+
+
+if __name__ == "__main__":
+    main()
